382f2aeb69a0a14d36be9215f93587083d91a04a
Shawnt Demirdjian <shawntdemirdjian@gmail.com>
2018-09-15 15:51:18 -0700

    Included Scripts to fix navbar. Changed admin@steeltotech to shawnt@steeltotech in footer.

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <footer class="w-100 text-center py-3">
-	<h6>Troubles? Tell Shawnt: <a href="mailto:admin@steeltotech.com">admin@steeltotech.com</a></h6>
+	<h6>Troubles? Tell Shawnt: <a href="mailto:shawnt@steeltotech.com">shawnt@steeltotech.com</a></h6>
 </footer>
